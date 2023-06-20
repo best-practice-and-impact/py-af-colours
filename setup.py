813bb23c8988f9a6260d139c9906ae674bb73f1a
Rosetta <Giselle.Rosetta@ons.gov.uk>
2023-06-20 10:50:57 +0100

    add package documentation

---
+++ setup.py
@@ -0,0 +1,11 @@
+# af_colours
+from setuptools import setup, find_packages
+
+setup(
+    name='af_colours',
+    version='0.1.0',
+    license='MIT',
+    author="Analysis standards and Pipelines team",
+    author_email='asap@ons.gov.uk',
+    url='https://github.com/best-practice-and-impact/af_colours/tree/main',
+)
