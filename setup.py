c840dc7a7175b2b9c712426dc19a551a8a7f753a
Giselle Rosetta <135234996+gisellerosetta@users.noreply.github.com>
2023-07-26 16:48:09 +0100

    change name of package, update docs (#10)
    * change name of package, update docs, change structure for pypi distribution

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-# af_colours
+# py_af_colours
 from setuptools import setup, find_packages
 
 setup(
-    name='af_colours',
+    name='py_af_colours',
     version='0.1.0',
     license='MIT',
     author="Analysis standards and Pipelines team",
     author_email='asap@ons.gov.uk',
-    url='https://github.com/best-practice-and-impact/af_colours/tree/main',
+    url='https://best-practice-and-impact.github.io/py-af-colours/',
+    install_requires=[],
+    packages=find_packages(),
 )
