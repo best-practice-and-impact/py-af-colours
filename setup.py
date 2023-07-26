d06248da4a35fffe4e1dd25a984e2a7bf520e49f
Rosetta <giselle.rosetta@ons.gov.uk>
2023-07-26 12:52:59 +0100

    change setup.py with underscore update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(
-    name='py-af-colours',
+    name='py_af_colours',
     version='0.1.0',
     license='MIT',
     author="Analysis standards and Pipelines team",
