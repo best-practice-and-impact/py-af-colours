cfbe39aa7c988f83daf7fd74d4d38dae57f71078
Rosetta <giselle.rosetta@ons.gov.uk>
2023-07-26 12:38:37 +0100

    change structure for pypi distribution

--- py-af-colours/setup.py
+++ py-af-colours/setup.py
@@ -1,8 +1,8 @@
-# af_colours
-from setuptools import setup, find_packages
+# py-af-colours
+from setuptools import setup
 
 setup(
-    name='af_colours',
+    name='py-af-colours',
     version='0.1.0',
     license='MIT',
     author="Analysis standards and Pipelines team",
