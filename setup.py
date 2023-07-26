a1a95030cb6aabc9eaadc3c6d1cba35f75762a8e
Rosetta <giselle.rosetta@ons.gov.uk>
2023-07-26 13:09:52 +0100

    change setup.py to add find_packages

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-# py-af-colours
-from setuptools import setup
+# py_af_colours
+from setuptools import setup, find_packages
 
 setup(
     name='py_af_colours',
@@ -7,5 +7,7 @@ setup(
     license='MIT',
     author="Analysis standards and Pipelines team",
     author_email='asap@ons.gov.uk',
-    url='https://github.com/best-practice-and-impact/af_colours/tree/main',
+    url='https://best-practice-and-impact.github.io/py-af-colours/',
+    install_requires=[],
+    packages=find_packages(),
 )
