6b1eb4887a5a6b72db259ce08692229b074958b5
Rosetta <giselle.rosetta@ons.gov.uk>
2023-08-03 11:21:41 +0100

    change data to alter graph axes to be continuous

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='py_af_colours',
-    version='0.1.0',
-    license='MIT',
-    author="Analysis standards and Pipelines team",
-    author_email='asap@ons.gov.uk',
-    url='https://best-practice-and-impact.github.io/py-af-colours/',
-    install_requires=[],
-    packages=find_packages(),
+    name = "py_af_colours",
+    version = "0.1.0",
+    license = "MIT",
+    author = "Analysis standards and Pipelines team",
+    author_email = "asap@ons.gov.uk",
+    url = "https://best-practice-and-impact.github.io/py-af-colours/",
+    install_requires = [],
+    packages = find_packages(),
 )
