6e7c7c0d3ac501ebdba115834811ec4a2d7d3145
Rosetta <giselle.rosetta@ons.gov.uk>
2023-09-14 15:32:36 +0100

    update toml and setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name = "py_af_colours",
     version = "0.1.0",
-    license = "MIT",
     author = "Analysis standards and Pipelines team",
     author_email = "asap@ons.gov.uk",
     url = "https://best-practice-and-impact.github.io/py-af-colours/",
