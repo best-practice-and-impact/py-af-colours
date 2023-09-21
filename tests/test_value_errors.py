83875d83c55e92898df04cc14e1a81651e68d6bf
Giselle Rosetta <135234996+gisellerosetta@users.noreply.github.com>
2023-09-21 11:15:10 +0100

    update unit tests (#24)

--- tests/test_value_errors.py
+++ tests/test_value_errors.py
@@ -1,17 +1,14 @@
 import pytest
-import yaml
 from py_af_colours import af_colours
 
-with open("../py_af_colours/config/config.yaml") as file:
-     config = yaml.load(file, Loader = yaml.BaseLoader)
+unit_test_config_path = "../py_af_colours/config/config.yaml"
 
-def check_value_errors():
+def test_value_errors():
     with pytest.raises(ValueError):
-        af_colours("wrong_palette", "hex")
-        af_colours("duo", "wrong_format")
-        af_colours("categorical", "hex", 0)
-        af_colours("categorical", "hex", 7)
-
+        af_colours("wrong_palette", "hex", config_path = unit_test_config_path)
+        af_colours("duo", "wrong_format", config_path = unit_test_config_path)
+        af_colours("categorical", "hex", 0, config_path = unit_test_config_path)
+        af_colours("categorical", "hex", 7, config_path = unit_test_config_path)
 
 # TODO: Try pytest parametrize:
 # Example use of af_colours:
@@ -19,12 +16,12 @@ def check_value_errors():
 # or introduce n, only used for categorical:
 # af_colours("categorical", "hex", n = 4)
 
-value_errors_test_cases = [("wrong_palette", "hex"),
-                           ("duo", "wrong_format"),
-                           ("categorical", "hex", 0),
-                           ("categorical", "hex", 7)]
+#value_errors_test_cases = [("wrong_palette", "hex"),
+#                           ("duo", "wrong_format"),
+#                           ("categorical", "hex", 0),
+#                           ("categorical", "hex", 7)]
 
-@pytest.mark.parametrize("af_colours_input", value_errors_test_cases)
-def test_mean_errors(af_colours_input):
-    with pytest.raises(ValueError):
-        af_colours(af_colours_input)
+#@pytest.mark.parametrize("af_colours_input", value_errors_test_cases)
+#def test_mean_errors(af_colours_input):
+#    with pytest.raises(ValueError):
+#        af_colours(af_colours_input)
