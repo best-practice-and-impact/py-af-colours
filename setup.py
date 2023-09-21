63e0b304e379e0d6c46168ac7ea79f9e3dea71f2
Rosetta <giselle.rosetta@ons.gov.uk>
2023-09-21 10:30:14 +0100

    change function name

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name = "py_af_colours",
-    version = "0.1.0",
+    version = "0.2.0",
     author = "Analysis standards and Pipelines team",
     author_email = "asap@ons.gov.uk",
     url = "https://best-practice-and-impact.github.io/py-af-colours/",
-    install_requires = [],
+    install_requires = [
+        "pyyaml"
+        ],
     packages = find_packages(),
 )
