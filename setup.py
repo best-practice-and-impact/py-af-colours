b477cad2080d7633beb8942899586deeddde2a57
Rosetta <giselle.rosetta@ons.gov.uk>
2023-10-05 14:41:18 +0100

    fix categorical 2 colour bug for rgb

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "py_af_colours",
-    version = "0.4.0",
+    version = "0.5.0",
     author = "Analysis standards and Pipelines team",
     author_email = "asap@ons.gov.uk",
     url = "https://best-practice-and-impact.github.io/py-af-colours/",
