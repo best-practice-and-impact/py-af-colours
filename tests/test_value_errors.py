5d8b2d14d093d2d5e2d3da5c06a28e6d735ad507
Rosetta <giselle.rosetta@ons.gov.uk>
2023-10-05 16:04:04 +0100

    add list length tests, add test for duo/categorical rgb

--- tests/test_value_errors.py
+++ tests/test_value_errors.py
@@ -3,25 +3,18 @@ from py_af_colours import af_colours
 
 unit_test_config_path = "./py_af_colours/config/config.yaml"
 
-def test_value_errors():
+def test_invalid_palette_value():
     with pytest.raises(ValueError):
         af_colours("wrong_palette", "hex", config_path = unit_test_config_path)
+
+def test_invalid_colour_format_value():
+    with pytest.raises(ValueError):
         af_colours("duo", "wrong_format", config_path = unit_test_config_path)
+
+def test_invalid_low_number_of_colours_value():
+    with pytest.raises(ValueError):
         af_colours("categorical", "hex", 0, config_path = unit_test_config_path)
+
+def test_invalid_high_number_of_colours_value():
+    with pytest.raises(ValueError):
         af_colours("categorical", "hex", 7, config_path = unit_test_config_path)
-
-# TODO: Try pytest parametrize:
-# Example use of af_colours:
-# af_colours("duo", "hex")
-# or introduce n, only used for categorical:
-# af_colours("categorical", "hex", n = 4)
-
-#value_errors_test_cases = [("wrong_palette", "hex"),
-#                           ("duo", "wrong_format"),
-#                           ("categorical", "hex", 0),
-#                           ("categorical", "hex", 7)]
-
-#@pytest.mark.parametrize("af_colours_input", value_errors_test_cases)
-#def test_mean_errors(af_colours_input):
-#    with pytest.raises(ValueError):
-#        af_colours(af_colours_input)
