c87f7b5e7d8c04a446442fab304b4fb887d51612
Rosetta <giselle.rosetta@ons.gov.uk>
2023-10-11 16:30:35 +0100

    update build settings

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "py_af_colours",
-    version = "1.0.4",
+    version = "1.0.5",
     author = "Analysis standards and Pipelines team",
     author_email = "asap@ons.gov.uk",
     url = "https://best-practice-and-impact.github.io/py-af-colours/",
@@ -11,4 +11,5 @@ setup(
         "pyyaml"
         ],
     packages = find_packages(),
+    include_package_data=True,
 )
